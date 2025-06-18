a108a1dbcad85575b1555354b64dd837940ba50e
karthikpk20046 <164026979+karthikpk20046@users.noreply.github.com>
2025-06-19 00:23:42 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,13 +1,11 @@
-# Problem-3.py
-
-def generate_odd_series_upto(n: int):
-    result = []
+def odd_up(n: int):
+    ans = []
     for i in range(1, n + 1):
         if i % 2 != 0:
-            result.append(i)
-    print(', '.join(map(str, result)))
+            ans.append(i)
+    print(', '.join(map(str, ans)))
 
 # Example usage:
-generate_odd_series_upto(11)
+odd_up(11)
 
 # OUTPUT: 1,3,5,7,9,11
